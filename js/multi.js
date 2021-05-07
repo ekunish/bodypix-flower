63a4bc6be1e58f40748eba8809ac235a5db45267
ekunish <ekunish@gmail.com>
2021-05-07 10:56:38 +0900

    花を出し分け

--- js/multi.js
+++ js/multi.js
@@ -1,6 +1,6 @@
 const main = async () => {
-    const resolution = { w: 1080, h: 720 };
-    const canvasSize = { w: 1080, h: 720 };
+    const resolution = {w: 1080, h: 720};
+    const canvasSize = {w: 1080, h: 720};
 
     const net = await bodyPix.load({
         architecture: 'MobileNetV1',
@@ -14,10 +14,10 @@ const main = async () => {
     navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
-            width: { ideal: resolution.w },
-            height: { ideal: resolution.h }
+            width: {ideal: resolution.w},
+            height: {ideal: resolution.h}
         }
-    }).then(function (stream) {
+    }).then((stream) => {
         video.srcObject = stream;
     });
 
@@ -27,14 +27,20 @@ const main = async () => {
 
     const ctx = output.getContext("2d");
 
-    const flowers = new Array()
+    let flowers = new Array()
+    let idNum = 0
 
     const hana = new Image();
     hana.src = "images/hana.png";
 
-    video.addEventListener('loadeddata', async() => {
+    const whiteGarbera = new Image();
+    whiteGarbera.src = "images/whiteGarbera.png";
+
+    video.addEventListener('loadeddata', async () => {
         setInterval(async () => {
+
             const bodies = await net.segmentMultiPerson(video);
+
             bodies && bodies.map((body) => {
                 const leftEye = body.pose.keypoints[1]
                 const rightEye = body.pose.keypoints[2]
@@ -42,30 +48,60 @@ const main = async () => {
                     const nose = body.pose.keypoints[0]
                     const scale = Math.abs(leftEye.position.x - rightEye.position.x)
                     const pause = Date.now()
+
+                    let id = 1
+                    let idExist = false
+                    flowers.length > 1 &&
+                        flowers.map((f) => {
+                            const dist = Math.sqrt(Math.pow((nose.position.x - f.x), 2) + Math.pow((nose.position.y - f.y), 2));
+                            // console.log(`nose.x: ${nose.position.x}`)
+                            // console.log(`f.x: ${f.x}`)
+                            // console.log(`dist: ${dist}`)
+                            // console.log(`f.scale: ${f.scale}`)
+                            if (dist < f.scale * 3) {
+                                id = f.id
+                                idExist = true
+                            }
+                        })
+                    if (flowers.length > 1 && !idExist) {
+                        id = idNum
+                        idNum += 1
+                    }
+
                     const flower = {
-                        x: output.width - nose.position.x - scale / 2,
-                        y: nose.position.y - scale / 2,
+                        x: nose.position.x,
+                        y: nose.position.y,
                         scale: scale,
-                        time: pause
+                        time: pause,
+                        id: id
                     }
                     flowers.push(flower);
                 }
             });
 
-            ctx.clearRect(0, 0, 1080, 720);
-            const stop = Date.now()
-            const drawFlowers = flowers
 
+            // clear
+            ctx.clearRect(0, 0, 1080, 720);
+
+            // draw
+            console.log(flowers)
+            flowers.map((f) => {
+                if (f.id % 2 != 0) {
+                    ctx.drawImage(hana, output.width - f.x - f.scale / 2, f.y - f.scale / 2, f.scale, f.scale)
+                }else{
+                    ctx.drawImage(whiteGarbera, output.width - f.x - f.scale / 2, f.y - f.scale / 2, f.scale, f.scale)
+                }
+            })
+
+            // renew
+            const now = Date.now()
             flowers.map((f, idx) => {
-                if (stop - f.time > 2000) {
+                if (now - f.time > 2000) {
                     flowers.splice(idx, 1)
                 }
             })
 
-            drawFlowers.map((f) => {
-                ctx.drawImage(hana, f.x, f.y, f.scale, f.scale)
-            })
-        }, 100)
+        }, 200)
 
     })
 }
